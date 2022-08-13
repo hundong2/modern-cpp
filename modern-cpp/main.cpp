b3f7918ad5038d67d9f6d6af9eca1946c8de9563
hundong2 <ehdgns104@gmail.com>
2022-08-13 09:07:39 +0900

    Initial Commit

---
+++ modern-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  modern-cpp
+//
+//  Created by songdong on 2022/08/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
