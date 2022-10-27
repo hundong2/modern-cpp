5d33617bd7b1e5bf17df535872cfc996fd68b901
hundong2 <ehdgns104@gmail.com>
2022-10-27 14:11:16 +0900

    util example folder added

---
+++ util/UtilProject/UtilProject/CBitSet.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bitset>
+#include <cstddef>
+
+namespace mbitset
+{
+	class CBitSet
+	{
+	public:
+		CBitSet();
+
+		/**
+		public variable 
+		*/
+	public:
+		std::byte b{ 42 };
+		int value;
+		void testPrint();
+		/*
+		Private Variable
+		*/
+	private:
+
+		
+	};
+}
